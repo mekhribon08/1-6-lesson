1b6b5254d64957212bc672ca63b5f6622b1afb0c
mekhribon08 <muxayyoxoljigitova79@gmail.com>
2023-01-08 17:39:11 +0500

    1-7-lesson

--- js/main.js
+++ js/main.js
@@ -50,3 +50,35 @@ elBtn.addEventListener("click", function (evt) {
     });
   });
 });
+
+document.addEventListener("click", (evt) => {
+  orModalBtnClick(evt);
+  onModalOutsideClick(evt);
+  onModalCloseClick(evt);
+});
+
+function orModalBtnClick(evt) {
+  const el = evt.target.closest("[data-modal-open]");
+
+  if (!el) return;
+
+  const modalSel = el.dataset.modalOpen;
+
+  document.querySelector(modalSel).classList.add("show");
+}
+
+function onModalOutsideClick(evt) {
+  const el = evt.target;
+
+  if (!el.matches("[data-modal]")) return;
+
+  el.classList.remove("show");
+}
+
+function onModalCloseClick(evt) {
+  const el = evt.target.closest("[data-modal-close]");
+
+  if (!el) return;
+
+  el.parentElement.parentElement.classList.remove("show");
+}
